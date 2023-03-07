f0d64dd310327fe2a9a69676e6b631416fce679f
Sebastian <sebastian@wisedigitalpartners.com>
2023-03-07 11:46:38 -0800

    fix

--- web/src/pages/reviews.js
+++ web/src/pages/reviews.js
@@ -1,24 +1,26 @@
+
 import React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import SearchEngineOptimization from "../components/SEO";
 import ButtonSolid from "../components/Button/ButtonSolid";
 import Hero from "../components/Hero/HeroFullWidth";
-import ReviewsGrid from "../components/Reviews/ReviewsGrid";
+import ReviewCards from "../components/Reviews/ReviewCards";
 
 const Page = ({ data }) => {
-  return (
-    <Layout headerHasBorder={true}>
-      <SearchEngineOptimization
-        title="Prodigy Plumbing Reviews | Long Beach & LA County"
-        description="With 1,500+ reviews from happy customers, Prodigy is the smart choice for a plumber and HVAC specialist in Long Beach & LA County. Get a free estimate today."
-        // openGraphImage={data.openGraphImage.publicURL}
-        // twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
-      />
+    return (
+      <Layout>
+        <SearchEngineOptimization
+          title="Reviews | Prodigy Plumbing"
+          openGraphImage={data.openGraphImage.publicURL}
+          twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
+        />
 
+        <section className="hidden md:block">
       <Hero
-        backgroundImages={data.heroDesktop.childImageSharp.gatsbyImageData}
+          backgroundImages={data.reviewsBack.childImageSharp.gatsbyImageData}
         backgroundPosition="0 50%"
         textMaxWidth="md:max-w-[720px]"
         padding="py-16 md:py-32"
@@ -31,31 +33,53 @@ const Page = ({ data }) => {
 
         <ButtonSolid href="/review-us/" text="Review Us" />
       </Hero>
+        </section>
+        <section className="block md:hidden bg-secondary-400 px-4">
+          <div className="flex justify-center items-center flex-col text-white pt-10 pb-14">
+            <h1 className="mb-6 font-light text-white">Testimonials</h1>
+            <p className="text-base text-center">
+              Don’t just take our word for it—hear feedback directly from our
+              clients.
+            </p>
 
-      <section className="pt-16 md:pt-24 pb-22 md:pb-16">
-        <div className="container">
-          <ReviewsGrid gridLayout="masonry" />
-        </div>
-      </section>
-    </Layout>
-  );
+            <ButtonSolid href="/reviews-us/" text="Leave a Review" />
+          </div>
+        </section>
+
+        <section className="pt-16 md:pt-20 pb-20 md:pb-32">
+          <div className="container">
+            <div >
+              <ReviewCards className="grid masonary" />
+            </div>
+          </div>
+        </section>
+
+
+      </Layout>
+    );
 };
 
 export const query = graphql`
   {
+
+    reviewsBack: file(relativePath: { eq: "5.2 Review/1.0Hero.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+      }
+    }
     openGraphImage: file(
-      relativePath: { eq: "open-graph/facebook/Reviews.jpg" }
+      relativePath: { eq: "open-graph/facebook/Homepage-Reviews.jpg" }
     ) {
       publicURL
     }
     twitterOpenGraphImage: file(
-      relativePath: { eq: "open-graph/twitter/Reviews.jpg" }
+      relativePath: { eq: "open-graph/twitter/Homepage-Reviews.jpg" }
     ) {
       publicURL
     }
-    heroDesktop: file(relativePath: { eq: "5.2 Review/1.0Hero.jpg" }) {
+    hero: file(relativePath: { eq: "5.2 Review/1.0Hero.jpg" }) {
       childImageSharp {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: NONE)
+        gatsbyImageData(layout: FULL_WIDTH)
       }
     }
   }
